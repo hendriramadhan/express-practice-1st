0809c3bf5e500a3f4a0082697e748611d177b163
hendriramadhan <hendriramadhan1101@gmail.com>
2022-12-25 14:07:11 +0700

    menambahkan crud sequelize

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const productRouter = require("./app/product/routes");
+//sequelize
+const productRouterV2 = require("./app/product2/routes");
+
 const res = require("express/lib/response");
 const logger = require("morgan");
 
@@ -10,6 +13,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/public", express.static(path.join(__dirname, "upload")));
 app.use("/api/v1", productRouter);
+//sequelize
+app.use("/api/v2", productRouterV2);
 
 app.use((req, res, next) => {
   // app.use((err, req, res, next) => {
